2e11383610473bc092cfa8dabe44703a3847677f
dtarroz <damien.tarroz.apps@gmail.com>
2024-08-30 07:18:03 +0200

    try(website):test web component simple

--- src/ItsMyDoliprane.Website/wwwroot/ts/components/button-test.ts
+++ src/ItsMyDoliprane.Website/wwwroot/ts/components/button-test.ts
@@ -1,7 +1,31 @@
 export class ButtonTest extends HTMLElement {
     constructor() {
         super();
-        this.innerHTML='<button>Testeur</button>';
+        this.attachShadow({ mode: 'open' });
+        this.shadowRoot!.innerHTML = `${this.styles()}<button>Testor</button>`;
+    }
+    
+    private styles(){
+        return `
+        <style>
+            button {
+                padding: 10px;
+                color: var(--background-color-default);
+                border-radius: 7px;
+                background-color: var(--button-color-default);
+                font-weight: bold;
+                font-size: 0.91rem;
+                cursor: pointer;
+                width: 126px;
+                border: none;
+                text-align: center;
+            }
+            
+            button:hover {
+                background-color: var(--button-color-hover-default);
+            }
+        </style>
+        `;
     }
 }
 
